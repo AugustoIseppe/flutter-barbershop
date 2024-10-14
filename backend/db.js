92aeb4be81e1bd77b3e12acfd2579f6055abe4fc
Augusto Iseppe <129420176+AugustoIseppe@users.noreply.github.com>
2024-10-14 16:09:02 -0300

    update app

--- backend/db.js
+++ backend/db.js
@@ -1,14 +1,14 @@
 import 'dotenv/config';
 import postgres from 'postgres';
 
-let { PGHOST, PGDATABASE, PGUSER, PGPASSWORD, ENDPOINT_ID } = process.env;
+let { PGHOST, PGDATABASE, PGUSER, PGPASSWORD, PGPORT, ENDPOINT_ID } = process.env;
 
 const sql = postgres({
   host: PGHOST,
   database: PGDATABASE,
   username: PGUSER,
   password: PGPASSWORD,
-  port: 5432,
+  port: PGPORT,
   ssl: 'require',
   connection: {
     options: `project=${ENDPOINT_ID}`,
